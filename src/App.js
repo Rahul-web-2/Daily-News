741fca9e1c9b4c73b1930280f260c4cd37eeb946
Rahul Lakhchaura <rlakhchaura3@gmail.com>
2024-09-02 13:49:52 +0530

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,39 +1,146 @@
-import React,{Component} from 'react';
-import { BrowserRouter as Router,Routes,  Route } from 'react-router-dom'
-import Newscomp from './Components/Newscomp'
-import Navbar from './Components/Navbar'
-//import './App.css'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Newscomp from './Components/Newscomp';
+import Navbar from './Components/Navbar';
+import './App.css';
 
-export default class App  extends Component {
-  pageSize= 6;
-  apiKey= process.env.REACT_APP_NEWS_API_KEY
-  country='in';
-  
-  render(){
+export default class App extends Component {
+  pageSize = 6;
+  apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  country = 'in';
+
+  render() {
     return (
       <>
-      <Router>
-        <Routes>
-          <Route  path="/" element={<Navbar title="Welcome" />}/>
-           <Route  path="/" element={<Newscomp  pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="General" />}/>
-          <Route  path="/Business" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Business" />}/>
-          <Route  path="/Entertainment" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Entertainment" />}/>
-      <Route  path="/Health" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Health" />}/>
-      <Route  path="/Science" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Science" />}/>
-      <Route  path="/Sports" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Sports" />}/>
-      <Route  path="/Technology" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} country={this.country} category="Technology" />}/>
-      <Route  path="/MoneyControl" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} domains={'moneycontrol.com'} category="MoneyControl" />}/>
-      <Route  path="/Aajtak" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} domains={'aajtak.in'} category="Aajtak" />}/> 
-      <Route exact path="/India" element={<Newscomp pageSize={this.pageSize} apiKey={this.apiKey} domains={'indiatoday.in'} category="India Today" />}/>
-      
-      </Routes>
-       
-      </Router>
-        
-        
-  
+        <Router>
+          {/* Navbar is placed outside of Routes so that it appears on every page */}
+          <Navbar title="Welcome" />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="General"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Business"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Business"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Entertainment"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Entertainment"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Health"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Health"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Science"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Science"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Sports"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Sports"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Technology"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  category="Technology"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/MoneyControl"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  domains="moneycontrol.com"
+                  category="MoneyControl"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/Aajtak"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  country={this.country}
+                  domains="aajtak.in"
+                  category="Aajtak"
+                />
+              }
+            />
+            <Route
+              exact
+              path="/India"
+              element={
+                <Newscomp
+                  pageSize={this.pageSize}
+                  apiKey={this.apiKey}
+                  domains="indiatoday.in"
+                  category="India Today"
+                />
+              }
+            />
+          </Routes>
+        </Router>
       </>
-    )
+    );
   }
-  
 }
